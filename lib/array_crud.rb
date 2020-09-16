627459ccc337cfdd93b20ab4cd7af8bf3676cad7
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-16 16:31:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  
+  arrays
+  maru_cat = famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
