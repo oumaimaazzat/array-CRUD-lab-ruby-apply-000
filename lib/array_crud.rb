f71355e66c0b782d02d463f0e25b379c392a8f95
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-16 16:31:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  "arrays!"
-  maru_cat = famous_cats.pop
+  "arrays!" = array.pop
 end
 
 def remove_element_from_start_of_array(array)
